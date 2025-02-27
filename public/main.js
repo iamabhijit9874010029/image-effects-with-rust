69009cfee05f38c3bff2bf3f2fb3c6769231030c
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2025-02-28 01:26:53 +0530

    Reading Files2

--- public/main.js
+++ public/main.js
@@ -3,6 +3,7 @@ function init() {
     const filereader = new FileReader();
 
     filereader.onloadend = () => {
+        // filereader.result holds the file contents after the file has been read using FileReader.readAsDataURL().
         let base64 = filereader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
         console.log(input.files[0]);
         console.log(base64);
