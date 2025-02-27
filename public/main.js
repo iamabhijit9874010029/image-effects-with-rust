e92a67f585aefb8919fa198bcd31b57987152b80
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2025-02-28 00:54:43 +0530

    Reading Files

--- public/main.js
+++ public/main.js
@@ -1,1 +1,17 @@
-console.log('test');
+function init() {
+    const input = document.getElementById('upload');
+    const filereader = new FileReader();
+
+    filereader.onloadend = () => {
+        let base64 = filereader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+        console.log(input.files[0]);
+        console.log(base64);
+
+    }
+
+    input.addEventListener('change', () => {
+        filereader.readAsDataURL(input.files[0]);
+    })
+}
+
+init();
