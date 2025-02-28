5c11312d507e3e88346fe442dcfa07372285f2ae
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2025-02-28 13:19:43 +0530

    Reading Files2

--- public/main.js
+++ public/main.js
@@ -1,17 +1,17 @@
 function init() {
     const input = document.getElementById('upload');
-    const filereader = new FileReader();
+    const fileReader = new FileReader();
 
-    filereader.onloadend = () => {
-        // filereader.result holds the file contents after the file has been read using FileReader.readAsDataURL().
-        let base64 = filereader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+    fileReader.onloadend = () => {
+        // fileReader.result holds the file contents after the file has been read using FileReader.readAsDataURL().
+        let base64 = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
         console.log(input.files[0]);
         console.log(base64);
 
     }
 
     input.addEventListener('change', () => {
-        filereader.readAsDataURL(input.files[0]);
+        fileReader.readAsDataURL(input.files[0]);
     })
 }
 
