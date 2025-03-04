d1140fc9c5417c05fab09e0488aac3a5e616c1e6
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2025-03-05 00:26:15 +0530

    Encoding an Image2

--- src/lib.rs
+++ src/lib.rs
@@ -13,12 +13,11 @@
 //     }
 // }
 
-
+use base64::{decode, encode};
+use image::ImageOutputFormat::Png;
+use image::load_from_memory;
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
-use base64::{encode, decode};
-use image::load_from_memory;
-use image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) -> String {
