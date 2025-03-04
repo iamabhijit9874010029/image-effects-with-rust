130269ae189cbec662bfb2b4b5b0e74d33c9c02d
iamabhijit9874010029 <abhijithaldar08@gmail.com>
2025-03-04 17:16:27 +0530

    Loading an Image from Memory

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 use base64::decode;
+use image::load_from_memory;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str){
@@ -24,4 +25,7 @@ pub fn grayscale(encoded_file: &str){
 
     let base64_to_vector = decode(encoded_file).unwrap();
     log(&"Image decoded".into());
+
+    let image = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded".into());
 }
